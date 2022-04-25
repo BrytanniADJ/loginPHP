e69df0d829e0f10d06c66722318afceacfe2714d
BrytanniADJ <99222703+BrytanniADJ@users.noreply.github.com>
2022-04-25 19:55:03 -0300

    up

--- lib/login.php
+++ lib/login.php
@@ -18,7 +18,7 @@ criptografia();
             if($usuarioOficial === 'admin' && $passOficial === '553c4a7cc063f5667404db3037e30ba9'){
                 header('Location: ./jogo.php');
             }else{
-                header('Location: ./home.php?login=erro');
+                header('Location: ../home.php?login=erro');
         }
     }login();
 }
